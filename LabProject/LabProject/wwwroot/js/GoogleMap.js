44413b1826e7e3e14e7f19acd226736a05a469f7
Filipe <Email do Filipe>
2020-12-13 18:21:05 +0000

    Main Page

---
+++ LabProject/LabProject/wwwroot/js/GoogleMap.js
@@ -0,0 +1,14 @@
+﻿// Initialize and add the map
+function initMap() {
+    // The location of Uluru
+    const uluru = { lat: 39.557191, lng: - 7.8536599 };
+
+    // The map, centered at Uluru
+    const map = new google.maps.Map(document.getElementById("map"), {
+        zoom: 7,
+        center: uluru,
+    });
+
+
+  
+}
